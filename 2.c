9c1e1b32be803de71ebb84d6d68eacc9a31310e8
eoxing <dbstjwls1002@gmail.com>
2022-10-12 19:53:13 +0900

    221012

---
+++ 2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void){
+    int a = 4;
+    int b = 5;
+
+    printf("%d+%d=%d\n", a, b, a+b);
+    printf("%d-%d=%d\n", a, b, a-b);
+    printf("%d*%d=%d\n", a, b, a*b);
+    printf("%d/%d=%d", a, b, a/b);
+
+    return 0;
+}
---
+++ 221012.c
@@ -0,0 +1,7 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void){
+    printf("이름 : 윤서진\n생일 : 10월 2일");
+    return 0;
+}
