93ce4d0b5ab1d3ad3f501b7936d6b085102709dc
leeseojun34 <leeseojun34@gmail.com>
2022-10-12 20:04:23 +0900

    Day02

---
+++ ex2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+	int a = 5;
+	int b = 4;
+	printf("%d+%d=%d", a, b, a + b);
+	printf("%d-%d=%d", a, b, a - b);
+	printf("%d*%d=%d", a, b, a * b);
+	printf("%d/%d=%d", a, b, a / b);
+
+	return 0;
+}
