6d7a1b6343331b75e19b730a9cd2c1c748469f37
jjjsun <ddorri83@naver.com>
2022-10-12 20:07:52 +0900

    test

--- 실습1.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#define _CRT_SECURE_NO_WARNINGS
-
-int main()
-{
-	printf("이름 : 이서연\n생일 : 11월 28일\n");
-}
--- 실습3.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-	printf("%s,%s,%s", "박재선", "컴퓨터과학과", "202210924");
-	return 0;
-}
