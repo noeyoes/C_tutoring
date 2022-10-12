ba57b69a9feccbf6d63ee0ecd4d6b7302225ab64
jjjsun <ddorri83@naver.com>
2022-10-12 20:12:29 +0900

    test

---
+++ 10월12일 튜터링/10월12일 튜터링/실습2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+int main()
+{
+	int a = 5;
+	int b = 4;
+	printf("%d + %d = %d\n", a, b, a+b);
+	printf("%d - %d = %d\n", a, b, a-b);
+	printf("%d * %d = %d\n", a, b, a*b);
+	printf("%d / %d = %d\n", a, b, a/b);
+	return 0;
+
+}
---
+++ 10월12일 튜터링/10월12일 튜터링/실습3.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("%s,%s,%s", "박재선", "컴퓨터과학과", "202210924");
+	return 0;
+}
