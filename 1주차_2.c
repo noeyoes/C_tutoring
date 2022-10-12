252a36d3497dadab3be0dac998fc8d5705119147
JANGJEEYEON <jjiyeon27@gmail.com>
2022-10-12 20:07:17 +0900

    1주차

---
+++ 1주차_2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int a = 5, b = 4;
+	printf("%d+%d=%d \n", a, b, a + b);
+	printf("%d-%d=%d \n", a, b, a - b);
+	printf("%d*%d=%d \n", a, b, a * b);
+	printf("%d/%d=%d \n", a, b, a / b);
+	return 0;
+}
---
+++ 1주차_3.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(void)
+{
+	printf("이름 : %s, 전공 : %s, 학년 : %s", "장지연", "컴퓨터과학전공", "1");
+	return 0;
+}
