ed7d65618b19afbe430a4f2ad577fa52bb9bae68
kangho <kil0653@naver.com>
2022-10-12 20:05:38 +0900

    제출

---
+++ 2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+	printf("%d+%d=%d\n", 5, 4, 9);
+	printf("%d-%d=%d\n", 5, 4, 1);
+	printf("%d*%d=%d\n", 5, 4, 20);
+	printf("%d/%d=%d\n", 5, 4, 1);
+	return 0;
+}
---
+++ 3.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void)
+{
+	printf("이름:%s\n전공:%s\n학년:%d학년", "김강호", "컴퓨터과학", 1);
+
+	return 0;
+}
