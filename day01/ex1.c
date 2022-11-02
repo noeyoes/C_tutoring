debca751552de990a6a69cae0e8f19812b72c11d
이서준 <106153233+leeseojun34@users.noreply.github.com>
2022-11-02 18:48:30 +0900

    실습 제출

---
+++ day01/ex1.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+    printf("이름 : 이서준\n생일 : 04월 27일");
+
+    return 0;
+}
--- ex1.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    printf("이름 : 이서준\n생일 : 04월 27일");
-
-    return 0;
-}
