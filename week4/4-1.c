e6ab0eaf504d1b5154cb0714fbea446c482b986b
eoxing <dbstjwls1002@gmail.com>
2022-11-02 11:20:50 +0900

    4주차 실습

---
+++ week4/4-1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+	int x, y;
+	int result;
+
+	printf("두 개의 정수를 입력하세요: ");
+	scanf("%d %d", &x, &y);
+
+	if (x > y)
+		result = x - y;
+	else
+		result = y - x;
+
+	printf("결과는 %d\n", result);
+}
