07be290fded31ffd13673c5b811159fe20da9271
foxihyun <tlgus0514@naver.com>
2022-11-02 14:47:42 +0900

    초기화

--- week1/prac_1.c
+++
@@ -1,5 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    printf("이름: 이서연 \n생일: 11월 28일 \n");
-}
--- week1/prac_2.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    printf("%d+%d=%d\n", 5, 4, 5+4);
-    printf("%d-%d=%d\n", 5, 4, 5-4);
-    printf("%d*%d=%d\n", 5, 4, 5*4);
-    printf("%d/%d=%d\n", 5, 4, 5/4);
-    return 0;
-}
