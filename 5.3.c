87ffaa67ec2a2ad9a2aebb584d31f8c82fc19b07
김강호 <114740506+kimkangho@users.noreply.github.com>
2022-11-16 19:22:24 +0900

    5주차 실습

---
+++ 5.3.c
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h> 
+
+int main(void)
+{
+	int num;
+	printf("정수입력: ");
+	scanf("%d", &num);
+	for (int i = 0; i < num; i++)
+	{
+		for (int j = i; j < num - 1; j++)
+		{
+			printf(" ");
+		}
+		for (int h = 0; h < i * 2 + 1; h++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+	return 0;
+
+}
