811955f16a2d8b889508bae8bd745caca1d699f1
ChocoChip0519 <tomcat0519@naver.com>
2022-11-16 17:50:58 +0900

    5주차
    제출

---
+++ 5주차/practice1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+ 
+int main()
+{
+    int i = 2;
+    
+        for (int j = 1; j <= 9; j++)
+        {
+            printf("%d x %d = %d\n", i, j, (i*j));
+        }
+        printf("\n");
+    return 0;
+ 
+
+}
---
+++ 5주차/practice5.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    int i, j, k = 0;
+
+    while (1) {
+        printf("== 트리 그리기 프로그램 ==\n");
+        printf("트리의 높이를 입력하세요(범위 5~15): ");
+        scanf("%d", &n);
+        if (n < 5)
+            printf("트리의 높이가 너무 낮습니다.\n");
+        else if (n >15)
+            printf("트리의 높이가 너무 높습니다.\n");
+        else {
+            for (i = 1; i <= n; i++) {
+                for (j = i; j < n; j++) {
+                    printf(" ");
+                }
+                while (k != (2 * i-1)) {
+                    if (k == 0 || k == 2 * i-2)
+                        printf("*");
+                    else
+                        printf(" ");
+                    k++;
+                }
+                k = 0;
+                printf("\n"); // print next row
+            }   
+
+            for (i = 0; i < 2 * n-1; i++) {
+                printf("*");
+            }
+
+            printf("\n");
+
+            for(i = 1; i < 3; i++){ 
+                for (j = 0; j < (2*n-1)/2; j++) {
+                    printf(" ");
+                }   
+                printf("|\n");
+            }
+            break;
+        }
+    }
+}
