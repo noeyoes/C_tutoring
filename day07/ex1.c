f4d1d1f9d29b9c02bc4784720332ac4f6457b5fb
이서준 <106153233+leeseojun34@users.noreply.github.com>
2022-11-30 18:14:16 +0900

    7주차 실습 완료

---
+++ day07/ex1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void Convert(int num) {
+    printf("8진수 출력: %o\n", num);
+    printf("10진수 출력: %d\n", num);
+    printf("16진수 출력: %x\n", num);
+}
+
+int main1() {
+    int num;
+    printf("정수를 입력하세요: ");
+    scanf("%d", &num);
+
+    Convert(num);
+
+    return 0;
+}
